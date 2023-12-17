d0c013bbc932f527b2bcb978c55b719dcd906fa5
wisdomstar94 <wisdomstar94@gmail.com>
2023-12-17 13:48:03 +0900

    instrument-panel 에서 눈금자 수치 표시 기능 추가 (삼각함수 활용)

--- src/app/instrument-panel/page.tsx
+++ src/app/instrument-panel/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useAddEventListener } from "@wisdomstar94/react-add-event-listener";
-import { arc, interpolate, scaleLinear, select } from "d3";
+import { arc, interpolate, interpolateArray, nice, quantile, scaleLinear, select } from "d3";
 import { useCallback, useEffect, useRef, useState } from "react";
 
 // https://www.pinterest.co.kr/pin/311241024259236439/
@@ -23,6 +23,8 @@ export default function Page() {
   const [cornerRadius, setCornerRadius] = useState(14);
   const [startFillColor, setStartFillColor] = useState('#87e9ff');
   const [endFillColor, setEndFillColor] = useState('#e5880d');
+  const [displayLineUnitCount, setDisplayLineUnitCount] = useState(5);
+  const [unitAndLinePeriod, setUnitAndLinePeriod] = useState(10);
 
   const drawChart = useCallback(() => {
     const svg = svgRef.current;
@@ -135,47 +137,191 @@ export default function Page() {
       })
     ;
 
-    g
-      .selectAll(`path[data-title='bg-value-line']`)
+    const lineOuterRadius = outerRadius - (strokeWeight + unitAndLinePeriod);
+    const lineInnerRadius = lineOuterRadius - 5;
+    const lineStrokeWeight = lineOuterRadius - lineInnerRadius;
+
+    const gValueLineContainer = g.selectAll(`g[data-title='value-line-container']`)
       .data(Array.from<number>({ length: unitCount }))
       .join(
-        enter => enter.append('path'),
+        enter => enter.append('g'),
         update => update.attr('data-timestamp', Date.now().toString()),
         exit => exit.remove(),
       )
-      .attr('data-title', 'bg-unit')
+      .attr('data-title', 'value-line-container')
       .attr('data-index', function(data: number, index: number, targetElements: any) {
         return index;
       })
+    ; 
+
+    gValueLineContainer
       .nodes()
       .forEach((node, index) => {
-        // if (index !== 0) return;
-        // console.log('@node', node);
+        // path
         const fillAngle = unitAngle / 1.5;
 
         const angleInfo = getAngleInfo({ strokeWidthAngle, unitCount, unitAngle: fillAngle, minimumValue, maximumValue });
 
         const startAngle = angleInfo.startAngle + (index * fillAngle) + (index * angleInfo.emptyUnitAngle);
-        const endAgle = startAngle + fillAngle;
-
-        const lineOuterRadius = outerRadius - (strokeWeight + 10);
-        const lineInnerRadius = lineOuterRadius - 5;
+        const endAngle = startAngle + fillAngle;
 
         select(node)
+          .selectAll(`path[data-title='bg-value-line']`)
+          .data([NaN])
+          .join(
+            enter => enter.append('path'),
+            update => update.attr('data-timestamp', Date.now().toString()),
+            exit => exit.remove(),
+          )
+          .attr('data-title', 'bg-value-line')
+          .attr('data-index', index)
           .attr(
             'd', 
             arc<unknown>() 
               .innerRadius(lineInnerRadius) 
               .outerRadius(lineOuterRadius) 
               .startAngle(startAngle) 
-              .endAngle(endAgle)
+              .endAngle(endAngle)
               .cornerRadius(cornerRadius)
           )
           .style('fill', '#ddd')
           // .attr('transform', `translate(${translateX}, ${translateY})`)
         ;
+
+        // text
+        const arr = [0, unitCount - 1];
+        const linearGenerator = scaleLinear()
+          .domain([0, displayLineUnitCount - 1])
+          .range([0, 1])
+        ;
+        const quantileArr = Array.from({ length: displayLineUnitCount }).map((k, i) => Math.round(quantile(arr, linearGenerator(i)) ?? 100000));
+        if (!quantileArr.includes(index)) {
+          select(node)
+            .selectAll(`text[data-title='value-line-data']`)  
+            .remove();
+          return;
+        }
+
+        const valueLineDataLinearGenerator = scaleLinear()
+          .domain([0, unitCount - 1])
+          .range([0, maximumValue])
+        ;
+
+        console.log('@@[0, unitCount - 1]', [0, unitCount - 1]);
+        console.log('@@[0, maximumValue]', [0, maximumValue]);
+        console.log(`@valueLineDataLinearGenerator(${index})`, valueLineDataLinearGenerator(index));
+
+        const distance = outerRadius - padding - (strokeWeight + unitAndLinePeriod + lineStrokeWeight);
+        const currentIndexAtValue = valueLineDataLinearGenerator(index);
+        const degInfo = angleInfo.getDegInfo(startAngle, endAngle, currentIndexAtValue, distance);
+        console.log('@@@@', {
+          distance,
+          startAngle,
+          endAngle,
+        });
+
+        select(node)
+          .selectAll(`text[data-title='value-line-data']`)
+          .data([NaN])
+          .join(
+            enter => enter.append('text'),
+            update => update.attr('data-timestamp', Date.now().toString()),
+            exit => exit.remove(),
+          )
+          .attr('data-title', 'value-line-data')
+          .attr('data-index', index)
+          .attr('y', degInfo.y)
+          .attr('x', degInfo.x)
+          // .attr('transform', `translate(0, ${-(outerRadius - padding - (strokeWeight + unitAndLinePeriod + lineStrokeWeight))})`)
+          .selectAll(`tspan[data-title='value-line-data-tspan']`)
+          .data([NaN])
+          .join(
+            enter => enter.append('tspan'),
+            update => update.attr('data-timestamp', Date.now().toString()),
+            exit => exit.remove(),
+          )
+          .attr('data-title', `value-line-data-tspan`)
+          .text(currentIndexAtValue.toFixed(2))
+          .attr("text-anchor", "middle")
+          .attr("font-family", "sans-serif")
+          .attr("font-size", "10px")
+          .attr("fill", "#888")
+        ;
       })
     ;
+
+
+    // gValueLineContainer
+    //   .selectAll(`path[data-title='bg-value-line']`)
+    //   .data([NaN])
+    //   .join(
+    //     enter => enter.append('path'),
+    //     update => update.attr('data-timestamp', Date.now().toString()),
+    //     exit => exit.remove(),
+    //   )
+    //   .attr('data-title', 'bg-value-line')
+    //   .attr('data-index', function(data: number, index: number, targetElements: any) {
+    //     return index;
+    //   })
+    //   .nodes()
+    //   .forEach((node, index) => {
+    //     // if (index !== 0) return;
+    //     // console.log('@node', node);
+    //     const fillAngle = unitAngle / 1.5;
+
+    //     const angleInfo = getAngleInfo({ strokeWidthAngle, unitCount, unitAngle: fillAngle, minimumValue, maximumValue });
+
+    //     const startAngle = angleInfo.startAngle + (index * fillAngle) + (index * angleInfo.emptyUnitAngle);
+    //     const endAgle = startAngle + fillAngle;
+
+    //     select(node)
+    //       .attr(
+    //         'd', 
+    //         arc<unknown>() 
+    //           .innerRadius(lineInnerRadius) 
+    //           .outerRadius(lineOuterRadius) 
+    //           .startAngle(startAngle) 
+    //           .endAngle(endAgle)
+    //           .cornerRadius(cornerRadius)
+    //       )
+    //       .style('fill', '#ddd')
+    //       // .attr('transform', `translate(${translateX}, ${translateY})`)
+    //     ;
+    //   })
+    // ;
+
+    // gValueLineContainer
+    //   .selectAll(`text[data-title='value-line-data']`)
+    //   .data([NaN])
+    //   .join(
+    //     enter => enter.append('text'),
+    //     update => update.attr('data-timestamp', Date.now().toString()),
+    //     exit => exit.remove(),
+    //   )
+    //   .attr('data-title', 'value-line-data')
+    //   .text(function(d, index) {
+    //     console.log('@d', d);
+    //     console.log('@index', index);
+
+    //     const arr = [0, unitCount - 1];
+    //     const linearGenerator = scaleLinear()
+    //       .domain([0, displayLineUnitCount - 1])
+    //       .range([0, 1])
+    //     ;
+
+    //     const quantileArr = Array.from({ length: displayLineUnitCount }).map((k, i) => Math.round(quantile(arr, linearGenerator(i)) ?? 100000));
+    //     console.log(`@quantileArr...`, quantileArr);
+
+    //     if (!quantileArr.includes(index)) return null;
+
+    //     const valueLineDataLinearGenerator = scaleLinear()
+    //       .domain([0, unitCount - 1])
+    //       .range([0, maximumValue])
+    //     ;
+
+    //     return valueLineDataLinearGenerator(index);
+    //   })
+    // ;
 
     const gValueDisplayContainer = g
       .selectAll(`g[data-title='value-display-container']`)
@@ -269,7 +415,7 @@ export default function Page() {
     //   .attr('data-title', 'arrow-container')
     //   .selectAll(``)
     // ;
-  }, [cornerRadius, endFillColor, maximumValue, minimumValue, padding, startFillColor, strokeWeight, strokeWidthAngle, unitAngle, unitCount, value, valueUnit]);
+  }, [cornerRadius, displayLineUnitCount, endFillColor, maximumValue, minimumValue, padding, startFillColor, strokeWeight, strokeWidthAngle, unitAndLinePeriod, unitAngle, unitCount, value, valueUnit]);
 
   useAddEventListener({
     windowEventRequiredInfo: {
@@ -331,6 +477,9 @@ export default function Page() {
           <li className="w-full relative">
             endFillColor : <input className="inline-flex border border-gray-500 px-1" type="text" value={endFillColor} onChange={e => setEndFillColor(e.target.value)} />
           </li>
+          <li className="w-full relative">
+            displayLineUnitCount : <input className="inline-flex border border-gray-500 px-1" type="number" value={displayLineUnitCount} onChange={e => setDisplayLineUnitCount(Number(e.target.value))} />
+          </li>
         </ul>
       </div>
     </>
@@ -357,12 +506,14 @@ function getAngleInfo(params: {
 
   const strokeWidthPercent = getPercent(Math.PI * 2, strokeWidthAngle);
   const totalDeg = 360 * (strokeWidthPercent / 100);
+  const remainDeg = 360 - totalDeg;
   const startDeg = - (totalDeg / 2);
 
   const totalEmptyAngleValue = strokeWidthAngle - (unitCount * unitAngle);
   const emptyUnitAngle = totalEmptyAngleValue / (unitCount - 1);
 
-  const startAngle = -((strokeWidthAngle) / 2);
+  // const startAngle = -((strokeWidthAngle) / 2);
+  const startAngle = Math.PI + (((Math.PI * 2) - strokeWidthAngle) / 2);
 
   console.log('@startAngle', startAngle);
 
@@ -386,6 +537,57 @@ function getAngleInfo(params: {
     return endColorOpacityGenerator(index);
   }
 
+  function getDegInfo(angle1: number, angle2: number, value: number, distance: number) {
+    const angleArea = getAngleArea(angle1, angle2);
+    console.log('@angle1, angle1', `${angle1},${angle2}`);
+    console.log('@angleArea', angleArea);
+
+    const valuePercent = getPercent(maximumValue, value);
+    const deg = totalDeg * (valuePercent / 100);
+    let triangleDeg = 0;
+    let x = 0;
+    let y = 0;
+
+    switch(angleArea) {
+      case 'left-bottom': {
+        triangleDeg = (remainDeg / 2) + deg;
+        const sin = Math.sin((triangleDeg * Math.PI) / 180);
+        x = -(distance * sin);
+        const cos = Math.cos((triangleDeg * Math.PI) / 180);
+        y = (distance * cos);
+      } break;
+      case 'left-top': {
+        triangleDeg = deg - (90 - (remainDeg / 2));
+        const cos = Math.cos((triangleDeg * Math.PI) / 180);
+        x = -(distance * cos);
+        const sin = Math.sin((triangleDeg * Math.PI) / 180);
+        y = -(distance * sin);
+      } break;
+      case 'right-up': {
+        triangleDeg = 90 - (deg - (totalDeg / 2));
+        const sin = Math.sin((triangleDeg * Math.PI) / 180);
+        y = -(distance * sin);
+        const cos = Math.cos((triangleDeg * Math.PI) / 180);
+        x = (distance * cos);
+      } break;
+      case 'right-bottom': {
+        triangleDeg = 360 - ((remainDeg / 2) + deg);
+        const cos = Math.cos((triangleDeg * Math.PI) / 180);
+        y = (distance * cos);
+        const sin = Math.sin((triangleDeg * Math.PI) / 180);
+        x = distance * sin;
+      } break;
+    }
+
+    return {
+      angleArea,
+      deg,
+      triangleDeg,
+      x,
+      y,
+    };
+  }
+
   return {
     totalEmptyAngleValue,
     emptyUnitAngle,
@@ -394,9 +596,59 @@ function getAngleInfo(params: {
     startDeg,
     getFillAngleInfo,
     getEndColorOpacity,
+    getDegInfo,
   };
 }
 
 function getPercent(target: number, value: number) {
   return (value * 100) / target;
 }
+
+function getAngleArea(angle1: number, angle2: number) {
+  let period = angle2 - angle1;
+  if (period < 0) {
+    period = -period;
+  }
+
+  let realAngle1 = angle1 > (Math.PI * 2) ? angle1 % (Math.PI * 2) : angle1;
+  let realAngle2 = realAngle1 + period;
+
+  const middleAngle = realAngle1 + (period / 2);
+  console.log('@@@middleAngle', middleAngle);
+
+  if (middleAngle >= 0 && middleAngle <= (Math.PI / 2)) {
+    return 'right-up';
+  }
+
+  if (middleAngle >= (Math.PI / 2) && middleAngle <= Math.PI) {
+    return 'right-bottom';
+  }
+
+  if (middleAngle >= Math.PI && middleAngle <= ((Math.PI * 2) * (3 / 4))) {
+    return 'left-bottom';
+  }
+
+  if (middleAngle >= ((Math.PI * 2) * (3 / 4)) && middleAngle <= (Math.PI * 2)) {
+    return 'left-top';
+  }
+
+  return '';
+}
+
+function getLineValueXY(angle1: number, angle2: number) {
+  let x = 0;
+  let y = 0;
+
+  const angleArea = getAngleArea(angle1, angle2);
+  switch(angleArea) {
+    case 'left-bottom': {
+
+    } break;
+  }
+
+  return {
+    x, 
+    y,
+  };
+}
+
